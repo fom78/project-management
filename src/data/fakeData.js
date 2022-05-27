06b11c21b6ac9311eddd54f6b19b0527371321d7
Fernando <fom78a@gmail.com>
2022-05-27 01:10:24 -0300

    add more fake data

--- src/data/fakeData.js
+++ src/data/fakeData.js
@@ -38,44 +38,89 @@ export const projects = [
     projectName: 'Landing Page',
     creationDate: '09/09/09 10:30',
     description: 'description',
-    assignedTo: 'Ignacio Truffan',
-    projectManager: 'Walt Cosani',
-    status: 'enabled'
+    assignedTo: users[3].name,
+    projectManager: users[1].name,
+    status: status[0].value
   },
   {
     id: 2,
     projectName: 'E-Commerce Shop',
     creationDate: '10/09/22 05:30',
     description: 'description',
-    assignedTo: 'Ignacio Truffa',
-    projectManager: 'Walt Cosani',
-    status: 'enabled'
+    assignedTo: users[0].name,
+    projectManager: users[2].name,
+    status: status[0].value
   },
   {
     id: 3,
     projectName: 'CRM Linkroom',
     creationDate: '07/18/21 17:00',
     description: 'description',
-    assignedTo: 'Ignacio Truffa',
-    projectManager: 'Walt Cosani',
-    status: 'enabled'
+    assignedTo: users[0].name,
+    projectManager: users[1].name,
+    status: status[0].value
   },
   {
     id: 4,
     projectName: 'Back Office google',
     creationDate: '05/24/22 21:00',
     description: 'description',
-    assignedTo: 'Castro Miriam',
-    projectManager: 'Perez Juan',
-    status: 'enabled'
+    assignedTo: users[3].name,
+    projectManager: users[2].name,
+    status: status[0].value
   },
   {
     id: 5,
     projectName: 'Challenge Esto Es',
     creationDate: '05/24/22 21:00',
     description: 'description',
-    assignedTo: 'Perez Juan',
-    projectManager: 'Castro Miriam',
-    status: 'enabled'
+    assignedTo: users[3].name,
+    projectManager: users[1].name,
+    status: status[0].value
+  },
+  {
+    id: 6,
+    projectName: 'Landing Page ONG',
+    creationDate: '04/30/22 10:30',
+    description: 'description',
+    assignedTo: users[3].name,
+    projectManager: users[1].name,
+    status: status[0].value
+  },
+  {
+    id: 7,
+    projectName: 'Fix Facebook site',
+    creationDate: '10/01/22 05:30',
+    description: 'description',
+    assignedTo: users[0].name,
+    projectManager: users[2].name,
+    status: status[1].value
+  },
+  {
+    id: 8,
+    projectName: 'Portal House',
+    creationDate: '05/28/21 17:00',
+    description: 'description',
+    assignedTo: users[0].name,
+    projectManager: users[1].name,
+    status: status[0].value
+  },
+  {
+    id: 9,
+    projectName: 'New feat SAP',
+    creationDate: '05/24/22 21:00',
+    description: 'description',
+    assignedTo: users[3].name,
+    projectManager: users[2].name,
+    status: status[0].value
+  },
+  {
+    id: 10,
+    projectName: 'Siemens web',
+    creationDate: '05/24/22 21:00',
+    description: 'description',
+    assignedTo: users[3].name,
+    projectManager: users[1].name,
+    status: status[1].value
   }
 ]
